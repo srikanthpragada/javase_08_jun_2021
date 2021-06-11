7333eb4a16619955c888bdd6290984d7f426c610
srikanthpragada <srikanthpragada@gmail.com>
2021-06-11 18:57:57 +0530

    11-JUN

---
+++ demo/src/PrintTable.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PrintTable {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter a number :");
+		int num = s.nextInt();
+
+		for (int i = 1; i <= 10; i++)
+			System.out.printf("%2d * %2d = %4d\n", num, i, num * i);
+
+	}
+
+}
